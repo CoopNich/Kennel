9197d94639f295df207e624e0d6eb8b83cc51f43
Cooper Nichols <coopnich92@gmail.com>
2020-03-02 15:10:39 -0600

    started

--- src/components/animal/AnimalEditForm.js
+++ src/components/animal/AnimalEditForm.js
@@ -3,74 +3,87 @@ import AnimalManager from "../../modules/AnimalManager"
 import "./AnimalForm.css"
 
 const AnimalEditForm = props => {
-  const [animal, setAnimal] = useState({ name: "", breed: "" });
-  const [isLoading, setIsLoading] = useState(false);
+    const [animal, setAnimal] = useState({ name: "", breed: "" });
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleFieldChange = evt => {
-    const stateToChange = { ...animal };
-    stateToChange[evt.target.id] = evt.target.value;
-    setAnimal(stateToChange);
-  };
-
-  const updateExistingAnimal = evt => {
-    evt.preventDefault()
-    setIsLoading(true);
-
-    // This is an edit, so we need the id
-    const editedAnimal = {
-      id: props.match.params.animalId,
-      name: animal.name,
-      breed: animal.breed
+    const handleFieldChange = evt => {
+        const stateToChange = { ...animal };
+        stateToChange[evt.target.id] = evt.target.value;
+        setAnimal(stateToChange);
     };
 
-    AnimalManager.update(editedAnimal)
-      .then(() => props.history.push("/animals"))
-  }
+    const updateExistingAnimal = evt => {
+        evt.preventDefault()
+        setIsLoading(true);
 
-  useEffect(() => {
-    AnimalManager.get(props.match.params.animalId)
-      .then(animal => {
-        setAnimal(animal);
-        setIsLoading(false);
-      });
-  }, []);
+        // This is an edit, so we need the id
+        const editedAnimal = {
+            id: props.match.params.animalId,
+            name: animal.name,
+            breed: animal.breed
+        };
 
-  return (
-    <>
-      <form>
-        <fieldset>
-          <div className="formgrid">
-            <input
-              type="text"
-              required
-              className="form-control"
-              onChange={handleFieldChange}
-              id="name"
-              value={animal.name}
-            />
-            <label htmlFor="name">Animal name</label>
+        AnimalManager.update(editedAnimal)
+            .then(() => props.history.push("/animals"))
+    }
 
-            <input
-              type="text"
-              required
-              className="form-control"
-              onChange={handleFieldChange}
-              id="breed"
-              value={animal.breed}
-            />
-            <label htmlFor="breed">Breed</label>
-          </div>
-          <div className="alignRight">
-            <button
-              type="button" disabled={isLoading}
-              onClick={updateExistingAnimal}
-              className="btn btn-primary"
-            >Submit</button>
-          </div>
-        </fieldset>
-      </form>
-    </>
-  );
+    useEffect(() => {
+        AnimalManager.get(props.match.params.animalId)
+            .then(animal => {
+                setAnimal(animal);
+                setIsLoading(false);
+            });
+    }, []);
+
+    return (
+        <>
+            <form>
+                <fieldset>
+                    <div className="formgrid">
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={handleFieldChange}
+                            id="name"
+                            value={animal.name}
+                        />
+                        <label htmlFor="name">Animal name</label>
+
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={handleFieldChange}
+                            id="breed"
+                            value={animal.breed}
+                        />
+                        <label htmlFor="breed">Breed</label>
+                        <select
+                            className="form-control"
+                            id="employeeId"
+                            value={animal.employeeId}
+                            onChange={handleFieldChange}
+                        >
+                            {employees.map(employee =>
+                                <option key={employee.id} value={employee.id}>
+                                    {employee.name}
+                                </option>
+                            )}
+                        </select>
+                        <label htmlFor="employeeId">Employee</label>
+                    </div>
+                    <div className="alignRight">
+                        <button
+                            type="button" disabled={isLoading}
+                            onClick={updateExistingAnimal}
+                            className="btn btn-primary"
+                        >Submit</button>
+                    </div>
+                </fieldset>
+            </form>
+        </>
+    );
 }
 
 export default AnimalEditForm
